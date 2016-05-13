49a721653ef756ecbe84ba8ec528d81a1855dd36
apoorvelec <apoorv@iitgn.ac.in>
2016-05-12 20:10:22 -0400

    A dynamic Hello World servlet.

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/work/Catalina/localhost/ServletExample/org/apache/jsp/Home_jsp.java
@@ -0,0 +1,82 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class Home_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List _jspx_dependants;
+
+  private javax.el.ExpressionFactory _el_expressionfactory;
+  private org.apache.AnnotationProcessor _jsp_annotationprocessor;
+
+  public Object getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _el_expressionfactory = _jspxFactory.getJspApplicationContext(getServletConfig().getServletContext()).getExpressionFactory();
+    _jsp_annotationprocessor = (org.apache.AnnotationProcessor) getServletConfig().getServletContext().getAttribute(org.apache.AnnotationProcessor.class.getName());
+  }
+
+  public void _jspDestroy() {
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+
+    try {
+      response.setContentType("text/html; charset=ISO-8859-1");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+
+      out.write("\r\n");
+      out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\r\n");
+      out.write("<html>\r\n");
+      out.write("<head>\r\n");
+      out.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\">\r\n");
+      out.write("<title>My first JSP</title>\r\n");
+      out.write("</head>\r\n");
+      out.write("<body>\r\n");
+      out.write("\t<form action = \"HelloServlet\">\r\n");
+      out.write("\tPlease enter a color and text<br>\r\n");
+      out.write("\tColor: <input type=\"text\" name=\"color\" size=\"20px\"><br>\r\n");
+      out.write("\tText: <input type=\"text\" name=\"text\" size=\"20px\"><br>\r\n");
+      out.write("\t<input type=\"submit\" value=\"submit\">\r\n");
+      out.write("\t</form>\r\n");
+      out.write("\r\n");
+      out.write("</body>\r\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          try { out.clearBuffer(); } catch (java.io.IOException e) {}
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else log(t.getMessage(), t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
---
+++ ServletExample/src/HelloWorld.java
@@ -0,0 +1,34 @@
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+public class HelloWorld extends HttpServlet { 
+  protected void doGet(HttpServletRequest request, 
+      HttpServletResponse response) throws ServletException, IOException 
+  {
+    // reading the user input
+    String color= request.getParameter("color");
+    String text= request.getParameter("text");
+    //response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    //out.print("Helo WOrld");
+    out.println (
+      "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n" +
+      "<html> \n" +
+        "<head> \n" +
+          "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\"> \n" +
+          "<title> My first jsp  </title> \n" +
+        "</head> \n" +
+        "<body> \n" +
+          "<font size=\"12px\" color=\"" + color + "\">" +
+            text +
+          "</font> \n" +
+        "</body> \n" +
+      "</html>" 
+    ); 
+  }  
+}
